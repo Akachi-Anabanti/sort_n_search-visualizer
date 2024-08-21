584e07a4736a13fbea8f4740d7d487d39d7792fc
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-08-21 10:37:54 +0100

    list of the algorithms

---
+++ src/utils/algorithmList.js
@@ -0,0 +1,17 @@
+export const sortAlgorithmList = [
+  "BubbleSort",
+  "SelectionSort",
+  "InsertionSort",
+  "MergeSort",
+  "QuickSort",
+  "HeapSort",
+  "RadixSort",
+];
+
+export const searchAlgorithmList = [
+  "Linear Search",
+  "Binary Search",
+  "Depth-First Search (DFS)",
+  "Breadth-First Search (BFS)",
+  "A* Search",
+];
